3996c99b4c50f26746ec60ea283c8d1bdb9be8c4
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-03-17 13:55:08 -0700

    Add first subclass.

--- Dickens/Subclasses/KKTextView.h
+++ Dickens/Subclasses/KKTextView.h
@@ -1,13 +1,13 @@
 //
-//  Dickens.h
+//  KKTextView.h
 //  Dickens
 //
 //  Created by Kolin Krewinkel on 3/17/13.
 //  Copyright (c) 2013 Handcrafted. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface Dickens : NSObject
+@interface KKTextView : UITextView
 
 @end
--- Dickens/Subclasses/KKTextView.m
+++ Dickens/Subclasses/KKTextView.m
@@ -1,13 +1,13 @@
 //
-//  Dickens.m
+//  KKTextView.m
 //  Dickens
 //
 //  Created by Kolin Krewinkel on 3/17/13.
 //  Copyright (c) 2013 Handcrafted. All rights reserved.
 //
 
-#import "Dickens.h"
+#import "KKTextView.h"
 
-@implementation Dickens
+@implementation KKTextView
 
 @end
