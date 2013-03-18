aeccca04058a72f27500243bbf123d34d2009ca5
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-03-17 17:31:14 -0700

    Add ellipsis expressions.

--- Dickens/Categories/NSString+KKPolishing.m
+++ Dickens/Categories/NSString+KKPolishing.m
@@ -10,15 +10,20 @@
 
 #pragma mark - Double Quotes
 
-NSString * const KKCharacterLeftDoubleQuotationMark = @"\u201c";
-NSString * const KKCharacterRightDoubleQuotationMark = @"\u201d";
-NSString * const KKPolishDoubleQuoteExpression = @"\".*\"";
+NSString *const KKCharacterLeftDoubleQuotationMark = @"\u201c";
+NSString *const KKCharacterRightDoubleQuotationMark = @"\u201d";
+NSString *const KKPolishDoubleQuoteExpression = @"\".*\"";
 
 #pragma mark - Single Quotes
 
-NSString * const KKCharacterLeftSingleQuotationMark = @"\u2018";
-NSString * const KKCharacterRightSingleQuotationMark = @"\u2019";
-NSString * const KKPolishSingleQuoteExpression = @"'.*'";
+NSString *const KKCharacterLeftSingleQuotationMark = @"\u2018";
+NSString *const KKCharacterRightSingleQuotationMark = @"\u2019";
+NSString *const KKPolishSingleQuoteExpression = @"'.*'";
+
+#pragma mark - Ellipsis
+
+NSString *const KKCharacterEllipsis = @"\u2026";
+NSString *const KKPolishingEllipsisExpression = @"(...)(?!’|”|'|\")";
 
 @implementation NSString (KKPolishing)
 
@@ -47,7 +52,7 @@ NSString * const KKPolishSingleQuoteExpression = @"'.*'";
     NSRegularExpression *doubleQuoteExpression = [[NSRegularExpression alloc] initWithPattern:KKPolishDoubleQuoteExpression options:NSRegularExpressionDotMatchesLineSeparators error:nil]; // Dot matches line seperators to include quotes which include linebreaks.
     for (NSTextCheckingResult *quoteResult in [doubleQuoteExpression matchesInString:self options:NSMatchingReportCompletion range:self.endToEndRange]) {
         NSString *content = [grammaticallyPolishedString substringWithRange:NSMakeRange(quoteResult.range.location + 1, quoteResult.range.length - 2)]; // Quoted content
-        [grammaticallyPolishedString replaceCharactersInRange:quoteResult.range withString:[self _wrapString:content withOpeningString:KKCharacterLeftDoubleQuotationMark closingString:KKCharacterRightDoubleQuotationMark]]; // Replace dumb quotes.
+        [grammaticallyPolishedString replaceCharactersInRange:quoteResult.range withString:[self _wrapString:content withOpeningString:KKCharacterLeftDoubleQuotationMark closingString:KKCharacterRightDoubleQuotationMark]]; // Replace dumb double quotes.
     }
 
     return [[NSString alloc] initWithString:grammaticallyPolishedString];
